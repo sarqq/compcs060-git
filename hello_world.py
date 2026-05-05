94e7f39f553a5b1cbccc12e5c5e38b09bd3ddadc
sarqq <75566902+sarqq@users.noreply.github.com>
2026-05-05 07:11:50 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 def main():
     print("Hello World!")
+    print("Hello from feature")
 
 if __name__ == "__main__":
     main()
-=======
-print("Hello world!")
->>>>>>> intermediate/master
